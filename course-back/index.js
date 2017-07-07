38a481d9597142d74a6221a2ae0847efde951c29
Siwakorn Thitisaksakul <clever_buffalo@hotmail.com>
2017-07-07 20:23:49 +0700

    something

--- course-back/index.js
+++ course-back/index.js
@@ -1,2 +1,64 @@
 
-console.log('kendo')
+var http = require('http')
+var MongoClient = require('mongodb').MongoClient
+
+
+console.log('Start the Server at Port 8080')
+//create a server object:
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'
+  , 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
+  , 'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
+  , 'Access-Control-Allow-Origin': '*'
+  , 'Access-Control-Max-Age': '86400'})
+
+  let payload = ''
+  req.on('data', function (data) {
+      payload += data;
+  });
+  req.on('end', function () {
+    console.log('====== got some request ========', req.method, payload)
+    if (req.url === '/login' && req.method === 'POST'){
+      if (!payload) {
+        res.write('login fail')
+        res.end()
+      } else {
+        const obj = JSON.parse(payload)
+        console.log('kendo jaa after', obj)
+        loginHandler(obj, res)
+      }
+    } else {
+      res.write('login fail')
+      res.end()
+    }
+  });
+
+}).listen(8080)
+
+const loginHandler = (payload, res) => {
+
+  const url = 'mongodb://localhost:27017/course'
+  const email = payload.email
+  const password = payload.password
+  console.log('kendo jaa payload', email, password)
+  MongoClient.connect(url, function(err, db) {
+    console.log('Connected to DB')
+    if (err) throw err
+    db.collection("users").findOne({ email }, function(err, result) {
+      if (err) throw err
+      db.close();
+      console.log('kendo jaa result',result)
+      if (result && result.password === password) {
+        console.log('======== login success ========')
+        res.write('login success')
+        console.log('kendo jaa omg fuck')
+        res.end()
+        console.log('kendo jaa omg')
+      } else {
+        console.log('kendo jaa herherhehrehrehrhehrehrherhehrehrherh')
+        res.write('login fail')
+        res.end()
+      }
+    });
+  });
+}
--- course-front/src/App.js
+++ course-front/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LoginPage from './LoginPage.js'
 import InApp from './InApp.js'
+import axios from 'axios'
 import './App.css'
 
 class App extends Component {
@@ -12,7 +13,25 @@ class App extends Component {
   }
 
   onLogin = (email, password) => {
-    console.log('kendo jaa', email, password)
+
+    var instance = axios.create({
+      baseURL: 'http://localhost:8080/',
+      timeout: 1000,
+      headers: {'content-Type': 'application/json'},
+    });
+    instance.post('/login', JSON.stringify({ email ,password }))
+      .then(function (response) {
+        console.log('kendo jaa 555', response.data, response.data === 'login success', typeof response.data)
+        if (response.data === 'login success') {
+          console.log('===== login success ======')
+          this.setState({ login: true })
+        } else {
+          console.log('===== login failed ======')
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
   }
 
   render () {
